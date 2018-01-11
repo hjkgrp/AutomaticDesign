79c3870a213da30fc9013b8f99ed235fca6b6a35
jpjanet <jpjanet@mit.edu>
2018-01-11 13:50:50 -0500

    Updated queue script

--- molSimplifyAD/sge_auto.sh
+++ molSimplifyAD/sge_auto.sh
@@ -4,33 +4,31 @@
 #$ -q gpus	        #gpus
 #$ -l gpus=1            #
 #$ -pe smp 1 		#number parrallel jobs
-#  -fin *.py
-#  -fin *.sh
-#  -fout scr    	# copy back to current dir
-#  -fout outfiles
-##################CHOOSE A METAL ######################
+
+module load cuda
+module load terachem
+
+
 fullpath="$1"
 
 echo "full path is $fullpath"
 generalpath=`echo $(dirname $fullpath) | sed "s,/*[^/]\*$,,"`
-echo "gen path is $generalpath"
+#echo "gen path is $generalpath"
 
 gennumpath=$(basename $generalpath)
-echo "gen path is $generalpath"
+#echo "gen path is $generalpath"
 
 generalpath=`echo $(dirname $generalpath) | sed "s,/*[^/]\*$,,"`
-echo "gen path is $generalpath"
+#echo "gen path is $generalpath"
 
 generalpath=`echo $(dirname $generalpath) | sed "s,/*[^/]\*$,,"`
 echo "gen path is $generalpath"
 
 namebase=`echo $fullpath | sed "s/[.]in//"| sed "s:.*/::"`
 
-echo "Begining Gibraltar geometry optimization run"
-echo "gen path is $generalpath"
+echo "Begining calcualtion run"
+echo "general path is $generalpath"
 export OMP_NUM_THREADS=1
-module load cuda
-module load terachem
 echo "gen path  $generalpath "
 echo " gen num path $gennumpath"
 echo "base name is  $namebase "
@@ -44,9 +42,9 @@ scrpath=$generalpath
 echo "scr will be copied to  $scrpath"
 echo "paths set"
 ## sett local, workdir related paths and copy files
-localoutpath=$SGE_O_WORKDIR/$namebase.out
-localinpath=$SGE_O_WORKDIR/$namebase.in
-coordfile=$SGE_O_WORKDIR/$namebase.xyz
+localoutpath=$namebase.out
+localinpath=$namebase.in
+coordfile=$namebase.xyz
 cp $initial_geo_path $coordfile 
 echo "copied"
 echo "inpath is $inpath"
@@ -57,8 +55,10 @@ spacer='_'
 echo "begining"
 echo "file is  $namebase"
 echo "this current home: $HOME"
-echo "this current env: $SGE_JOB_SPOOL_DIR"
+echo "this current SGE_JOB_SPOOL_DIR: $SGE_JOB_SPOOL_DIR"
 echo "this SGE WORKDIR: $SGE_O_WORKDIR"
+echo "this SGE_O_PATH: $SGE_O_PATH"
+echo "this SGE shell current DIR: $PWD"
 
 wf_guess_flag=0
 ##begin geo-optimization
@@ -82,6 +82,5 @@ terachem $localinpath >  $localoutpath
 echo "Complete"
 
 ## copy back complete cases 
-cp $localinpath /home/jp/tfac/issue.issue
 cp $localoutpath $outpath
 cp -r scr $scrpath
