90989833633253c7ec94f5941cd14b19ce8d8873
Aditya Nandy <aditya.nandy@gmail.com>
2018-07-30 18:39:03 -0400

    Store chem_name in get_distances so that human readable CSV can be generated later down the line

--- molSimplifyAD/get_distances.py
+++ molSimplifyAD/get_distances.py
@@ -10,6 +10,8 @@ from molSimplifyAD.ga_get_general import _get_gen_npool
 def _find_distances():
     lastgen,npool = _get_gen_npool(get_run_dir())
     gene_dist_dict = dict()
+    gene_prop_dict = dict()
+    gene_name_dict = dict()
     #lastgen is the last generation that has been run
     for generation in xrange(lastgen+1):
         ANN_path = get_run_dir() + "ANN_ouput/gen_" + str(generation) + "/ANN_results.csv"
@@ -22,25 +24,35 @@ def _find_distances():
                 runtype = GA_run.config["runtype"]
                 for i, line in enumerate(lol):
                     if i == 0:
-                        line_list = line.split(",")
+                        line_list = line.strip('\n').split(",")
+                        # print(line_list)
                         prop_idx = line_list.index(runtype)
                         dist_idx = line_list.index(runtype+'_dist')
                         continue
                     line_list = line.split(",")
-                    gene = line_list[0]
+                    job = line_list[0]
+                    gene, _, _, metal, ox, eqlig, axlig1, axlig2, _, _, _, spin, _, ahf, _, _ = translate_job_name(job)
+                    # print('gene',gene)
+                    # print('job',job)
                     prop = line_list[prop_idx]
                     dist = line_list[dist_idx]
-                    ll = gene.split("_")
+                    ll = job.split("_")
+
 #                    if runtype == "split":
                     geneName = "_".join(ll[4:10])
 #                    elif runtype == "redox":
 #                        geneName = "_".join(ll[4:9])
-                    
+                    # print('genename',geneName)
+                    metals = get_metals()
+                    metal = metals[metal]
+                    chem_name = '_'.join([str(metal),str(ox),'eq',str(eqlig),'ax1',str(axlig1),'ax2',str(axlig2),str(ahf),str(spin)])
                     dist = float(dist.strip('\n'))
                     if geneName in gene_dist_dict.keys():
                         pass
                     else:
                         gene_dist_dict.update({geneName:dist})
+                        gene_prop_dict.update({geneName:prop})
+                        gene_name_dict.update({geneName:chem_name})
                         #print geneName + " : " + str(distance)
 
             fi.close()
@@ -53,7 +65,7 @@ def _find_distances():
     if emsg:
             print(emsg)
 
-    return gene_dist_dict,npool
+    return gene_dist_dict,npool, gene_prop_dict, gene_name_dict
 
 def _mean_distances(gene_dist_dict):
     lastgen,npool = _get_gen_npool(get_run_dir())
@@ -86,5 +98,5 @@ def _mean_distances(gene_dist_dict):
 
 # Uses the same directory as get_general, which is get_run_dir() from ga_tools
 def format_distances():
-    gene_dist_dict,npool = _find_distances()
+    gene_dist_dict,npool,_,_ = _find_distances()
     _mean_distances(gene_dist_dict)
