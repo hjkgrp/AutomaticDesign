a2df3c04ad2d267e865e0ac2de192181509c6dfd
jpjanet <jpjanet@mit.edu>
2018-11-02 15:12:45 -0400

    added DB push script to utils (was left out of repo). fixed some imports on the demo visualizer

--- molSimplifyAD/utils/demo_support_functions.py
+++ molSimplifyAD/utils/demo_support_functions.py
@@ -1,4 +1,24 @@
-
+import numpy as np
+import scipy as sp
+import pandas as pd
+import sklearn as sk
+from sklearn import decomposition
+from matplotlib.pyplot import figure, imshow, axis
+from matplotlib.image import imread
+from molSimplifyAD.utils.report_tool.prepare_report import *
+from molSimplifyAD.ga_init import *
+from IPython.display import Image
+from pymol.cgo import *
+from pymol import cmd
+from pymol import preset
+from pymol import util
+import matplotlib.tri as tri
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+from IPython import display
+from scipy.spatial import ConvexHull
+from scipy.interpolate import UnivariateSpline
+from matplotlib import gridspec
 def draw3d_big(load_path, save_path):
     cmd.delete('all')
 
@@ -164,55 +184,19 @@ def  get_run_info(mad_path):
             mfs.append(float(ll[1]))
             divs.append(float(ll[2]))
     return gens, mfs, divs
-        
-def draw_pca_fig(Xr,mod_dat,live_db_inds,all_inds,hull):
-    fig = figure(figsize = (10,10))
-
-
-    font = {'family' : 'normal',
-            'weight' : 'bold',
-            'size'   : 16}
-
-    plt.rc('font', **font)
-    #colors = [(0.3, 0.3, 0.3),(0.95,0.3,1)]  
-    cmstr = 'winter'
-    #cm = LinearSegmentedColormap.from_list(
-    #    'atoms', colors,N=10)
-    points  = Xr[:,0:2]
-    for simplex in hull.simplices:
-        plt.plot(points[simplex, 0], Xr[simplex, 1], 'k-')
-
-    o1 = plt.tricontourf(Xr[all_inds,0], Xr[all_inds,1],mod_dat.loc[all_inds,'fitness'],
-                   5,alpha=0.25,cmap=cmstr,linestyles='dashed',linewidths=1)
-    for c in o1.collections:
-        c.set_edgecolor("face")
-
-    o2 = plt.scatter(Xr[all_inds,0], Xr[all_inds,1], c=mod_dat.loc[all_inds,"fitness"],
-            cmap=cmstr,s=100,
-                alpha=1, lw=1,marker=".",label='previous')
-    o3 = plt.scatter(Xr[live_db_inds,0], Xr[live_db_inds,1], c=mod_dat.loc[live_db_inds,"fitness"],
-            cmap=cmstr,s=125,
-                alpha=1.0, lw=3,marker="s",label='current')
-    cbar = fig.colorbar(o1)
-    cbar.ax.set_ylabel('fitness',fontweight='bold')
-    #cbar.add_lines(o1)
-    plt.xlabel('PC1',fontweight='bold')
-    plt.ylabel('PC2',fontweight='bold')
-    plt.legend()
-    frame1 = plt.gca()
-    frame1.axes.xaxis.set_ticklabels([])
-    frame1.axes.yaxis.set_ticklabels([])
-    frame1.axes.tick_params(direction='in')
-    plt.show()
 
 def draw_double_plot(Xr,mod_dat,live_db_inds,all_inds,hull,gens,mfs,divs):
+    font = {'weight' : 'bold',
+            'size'   : 18}
+
+    plt.rc('font', **font)
     #fig, axs = plt.subplots(nrows=1,ncols=2,figsize = (18,9))
     fig = figure(figsize=(20,10))
     gs = gridspec.GridSpec(1, 2, width_ratios=[1.75, 1]) 
     ax1 =  plt.subplot(gs[0])
     ax2 =  plt.subplot(gs[1])
     font = {'weight' : 'bold',
-            'size'   : 16}
+            'size'   : 18}
 
     plt.rc('font', **font)
     #colors = [(0.3, 0.3, 0.3),(0.95,0.3,1)]  
@@ -224,42 +208,41 @@ def draw_double_plot(Xr,mod_dat,live_db_inds,all_inds,hull,gens,mfs,divs):
         ax1.plot(points[simplex, 0], Xr[simplex, 1], 'k-')
 
     o1 = ax1.tricontourf(Xr[all_inds,0], Xr[all_inds,1],mod_dat.loc[all_inds,'fitness'],
-                   5,alpha=0.25,cmap=cmstr,linestyles='dashed')
+                   5,alpha=0.1,cmap=cmstr,linestyles='dashed', vmin=0, vmax=1)
     for c in o1.collections:
         c.set_edgecolor("face")
 
     o2 = ax1.scatter(Xr[all_inds,0], Xr[all_inds,1], c=mod_dat.loc[all_inds,"fitness"],
             cmap=cmstr,s=100,
-                alpha=1, lw=1,marker=".",label='previous')
+                alpha=1, lw=1,marker=".",label='previous', vmin=0, vmax=1)
     o3 = ax1.scatter(Xr[live_db_inds,0], Xr[live_db_inds,1], c=mod_dat.loc[live_db_inds,"fitness"],
             cmap=cmstr,s=125,
-                alpha=1.0, lw=3,marker="s",label='current')
+                alpha=1.0, lw=3,marker="s",label='current', vmin=0, vmax=1,
+                    edgecolor='black', linewidth=2)
     cbar = fig.colorbar(o1,ax=ax1)
     cbar.ax.set_ylabel('fitness',fontweight='bold')
     #cbar.add_lines(o1)
     ax1.set_xlabel('PC1',fontweight='bold')
     ax1.set_ylabel('PC2',fontweight='bold')
     ax1.legend()
-    frame1 = plt.gca()
-    frame1.axes.xaxis.set_ticklabels([])
-    frame1.axes.yaxis.set_ticklabels([])
-    frame1.axes.tick_params(direction='in')
+    ax1.set_xlim(-19,21)
+    ax1.set_ylim(-17,27)
+    ax1.set_yticklabels([])
+    ax1.set_xticklabels([])
 
+    
 
-
+    ## second plot
     ax3 = ax2.twinx()
-
     ax2.plot(gens, mfs, 'go',markersize=6)
     ax3.plot(gens, divs, 'bs',markersize=6)
 
-    xnew = np.linspace(0,max(gens),500)
-
-    mfs_spl = UnivariateSpline(gens, mfs,s=100)
-    divs_spl = UnivariateSpline(gens, divs,s=100)
-
-
-    ax2.plot(xnew, mfs_spl(xnew), 'g--',lw=4.5)
-    ax3.plot(xnew, divs_spl(xnew), 'b--',lw=4.5)
+    if len(mfs) > 3:
+        xnew = np.linspace(0,max(gens),500)
+        mfs_spl = UnivariateSpline(gens, mfs,s=100)
+        divs_spl = UnivariateSpline(gens, divs,s=100)
+        ax2.plot(xnew, mfs_spl(xnew), 'g--',lw=4.5)
+        ax3.plot(xnew, divs_spl(xnew), 'b--',lw=4.5)
 
 
     ax2.set_xlabel('generation',fontweight='bold')
@@ -269,5 +252,9 @@ def draw_double_plot(Xr,mod_dat,live_db_inds,all_inds,hull,gens,mfs,divs):
     plt.setp(ax3.get_yticklabels(), color="b")
     ax2.yaxis.set_tick_params(color='b')
 
+    font = {'weight' : 'bold',
+            'size'   : 18}
+
+    plt.rc('font', **font)
 
     plt.show()
