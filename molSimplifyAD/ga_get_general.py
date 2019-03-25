9c6d5e29229a1b5633980d882aff943e5a1dc91f
Aditya Nandy <aditya.nandy@gmail.com>
2019-03-25 15:59:39 -0400

    update get general

--- molSimplifyAD/ga_get_general.py
+++ molSimplifyAD/ga_get_general.py
@@ -91,16 +91,36 @@ def _human_readable_csv(base_path, generation, end_results):
     from molSimplifyAD.ga_main import *
     gene_dist_dict, _, gene_prop_dict, gene_name_dict = _find_distances()
     csv_results_path = base_path + "human_readable_results.csv"
+    print('AT HRCSV!!!!')
     with open(csv_results_path, _write_mode(generation)) as fo:
         writer = csv.writer(fo)
+        # print('didnt fail here')
         if int(generation) == 0:
             writer.writerow(('Generation', 'Gene', 'Chem Name', 'Fitness', 'Property', 'Distance', 'Frequency','Fitness Function','Dist Parameter'))
         else:
             writer.writerow(('\n'))
-        for i in range(len(end_results)):
-            t = end_results[i]
+            # print('didnt fail here2')
+        for i, val in enumerate(end_results):
+            # t = end_results[i]
+            # print('didnt fail here3')
+            # print(i, len(end_results))
+            # print(gene_name_dict)
+            # print([x.fitness for x in end_results])
+            t = val
+            # print(t.generation) #CR V NOT GETTING ASSIGNED TO THE GENE NAME DICT--> problematic!!!
+            # print(t.name)
+            # print(t.fitness_function)
+            # print(t.name in gene_name_dict.keys())
+            # print(gene_name_dict[t.name])
+            # print(t.fitness)
+            # print(gene_prop_dict[t.name])
+            # print(gene_dist_dict[t.name])
+            # print(t.frequency)
+            # print(t.dist_param)
+            # print('-!-!-!-!-!-!-!-')
             writer.writerow((t.generation, t.name, gene_name_dict[t.name], t.fitness, gene_prop_dict[t.name],
                              gene_dist_dict[t.name], t.frequency, t.fitness_function, t.dist_param))
+    # print('hereeee')
     fo.close()
 
 
@@ -135,9 +155,12 @@ def _get_freq_fitness(lastgen, npool):
             ANN_dir = isKeyword('rundir') + "ANN_ouput/gen_" + str(generation) + "/ANN_results.csv"
             emsg, ANN_dict = read_ANN_results_dictionary(ANN_dir)
             for keys in ANN_dict.keys():
-                translate_dict = translate_job_name(jobs)
+                # print('got here')
+                print(ANN_prop_dict.keys())
+                translate_dict = translate_job_name(keys)
                 this_gene = translate_dict['gene']
                 metal = translate_dict['metal']
+                ox = translate_dict['ox']
                 spin = translate_dict['spin']
                 spin_cat = translate_dict['spin_cat']
                 # this_gene = "_".join(keys.split("_")[4:10])
@@ -146,13 +169,23 @@ def _get_freq_fitness(lastgen, npool):
                 # elif runtype in ['oxo','hat']:
                 #     this_gene = "_".join(keys.split("_")[4:10])
                 split_energy = ANN_dict[keys]['split']
-                if runtype in ['oxo','hat']:
+                if runtype in ['split']:
+                    this_prop = float(ANN_dict[keys][runtype])
+                    this_dist = float(ANN_dict[keys][runtype + '_dist'])
+                    ANN_prop_dict.update({this_gene: this_prop})
+                elif runtype in ['oxo','hat']:
+                    # print('got here 3')HR CSVHR CSV
                     if not (this_gene in ANN_prop_dict.keys()):
+                        # print('got here 2')
                         this_prop = float(ANN_dict[keys][runtype])
                         this_dist = float(ANN_dict[keys][runtype + '_dist'])
                         # if spin_cat == 'HS' or (metal == 'cr' and int(spin) == 2):
-                        if spin_cat == 'LS':
+                        if spin_cat == isKeyword('spin_constraint'):
+                            # print('updated ANN propdict')
                             ANN_prop_dict.update({this_gene: this_prop})
+                        elif isKeyword('spin_constraint') and get_metals()[metal].lower() == 'cr' and ox == 5:
+                            print('Cr(V) does not exist in HS')
+                            ANN_prop_dict.update({this_gene: 10000})
                         else:
                             # ANN_prop_dict.update({this_gene: 10000})
                             print('SKIPPED ANN PROP DICT ASSIGNMENT BC '+str(this_gene)+' '+str(spin))
@@ -161,6 +194,21 @@ def _get_freq_fitness(lastgen, npool):
                         this_prop = float(ANN_dict[keys][runtype])
                         this_dist = float(ANN_dict[keys][runtype + '_dist'])
                         ANN_prop_dict.update({this_gene: this_prop})
+                elif type(runtype) == list:
+                    this_prop = []
+                    this_dist = []
+                    for run in runtype:
+                        this_prop.append(float(ANN_dict[keys][run]))
+                        this_dist.append(float(ANN_dict[keys][run + '_dist']))
+                    if spin_cat == isKeyword('spin_constraint'): #Constraining this to a single spin state.
+                        print('freq fitness multiple prop update')
+                        ANN_prop_dict.update({this_gene: this_prop})
+                    elif isKeyword('spin_constraint') and get_metals()[metal].lower() == 'cr' and ox == 5:
+                        print('Cr(V) does not exist in HS')
+                        ANN_prop_dict.update({this_gene: [10000, 10000]})
+                    else:
+                        # ANN_prop_dict.update({this_gene: 10000})
+                        print('SKIPPED ANN PROP DICT ASSIGNMENT BC '+str(this_gene)+' IS SPIN '+str(spin))
                 # elif not (this_gene in ANN_prop_dict.keys()):
                 #     sardines
                 #     this_prop = float(ANN_dict[keys][runtype])
@@ -192,10 +240,13 @@ def _get_freq_fitness(lastgen, npool):
         print("opened Gen: " + str(generation))
         for line in fi:
             # print(fi.readline())
-            print(line)
             geneName = line.split(",")[1]
-            dist_param = line.split(",")[2].strip('\n')
-            fit_func = line.split(",")[3].strip('\n')
+            if len(line.split(",")) == 5:
+                dist_param = (",".join(line.split(",")[2:4])).strip('\n')
+                fit_func = line.split(",")[4].strip('\n')
+            else:
+                dist_param = line.split(",")[2].strip('\n')
+                fit_func = line.split(",")[3].strip('\n')
             geneName = geneName.strip('\n')
             # print('GET GENERAL GENENAME:',geneName)
             current_gene_list.append(geneName)
@@ -204,6 +255,8 @@ def _get_freq_fitness(lastgen, npool):
                 end_results[index].frequency += 1
             else:
                 end_results.append(gene(geneName, 0, 1, generation, fit_func, dist_param))
+        print('printing end results')
+        print([val.name for val in end_results], len([val.name for val in end_results]))
         fi.close()
         print('got to this point 1')
         # Second, find fitness values of genes, add to list, and calculate mean fitness.
@@ -217,20 +270,20 @@ def _get_freq_fitness(lastgen, npool):
                 geneName, _ = line.split(",",1)
                 prop = ANN_prop_dict[geneName]
                 if 'hinge' in str(isKeyword('scoring_function')):
-                    fitness = find_prop_hinge_fitness(prop, int(isKeyword('property_parameter')))
+                    fitness = find_prop_hinge_fitness(prop, isKeyword('property_parameter'))
                 else:
                     fitness = find_prop_fitness(prop, isKeyword('property_parameter'))
 
                 # fitness = fitness.strip('\n')
-                print('FITNESS ASSIGNED', fitness)
+                # print('FITNESS ASSIGNED', fitness)
+                # print(end_results)
                 index = _find_gene(geneName, end_results)
                 if index >= 0:
                     temp = end_results[index]
                     sumt += temp.frequency * float(fitness)
                     temp.fitness = format(float(fitness), '.12f')
                     print(temp._long())
-        fi.close()
-
+        print('got to this point 2')
         ## Write a summary file for the property, distance, and fitness.
         # print('_____________________________________________________________________Entered part 3.')
         sum_results = []
@@ -245,17 +298,20 @@ def _get_freq_fitness(lastgen, npool):
             else:
                 sum_results.append(gene(geneName, 0, 1, generation, None, None))
         fi.close()
-
         # Third, output the unique genes and their fitness values to .txt and .csv files.
         _write_all_txt(base_path, generation, end_results)
         _write_all_csv(base_path, generation, end_results)
         _gen_gene_fitness_csv(base_path, generation, end_results, sumt)
         _human_readable_csv(base_path, generation, end_results)
+        print('here')
         _write_summary_csv(base_path, generation, sum_results)
-
+        
         # Fourth, recover actual splitting energies only in ANN case
         if not isKeyword('DFT'):
-            mean_prop = 0.0
+            if type(runtype) == list:
+                mean_prop = [0.0, 0.0]
+            else:
+                mean_prop = 0.0
             count = 0
             print('ANN keys are ')
             print(ANN_prop_dict.keys())
@@ -267,6 +323,9 @@ def _get_freq_fitness(lastgen, npool):
                         if runtype == 'split':
                             mean_prop += abs(ANN_prop_dict[geneName])
                             count += 1
+                        elif type(runtype) == list:
+                            mean_prop[0] += ANN_prop_dict[geneName][0]
+                            mean_prop[1] += ANN_prop_dict[geneName][1]
                         else:
                             mean_prop += ANN_prop_dict[geneName]
                             count += 1
@@ -274,7 +333,11 @@ def _get_freq_fitness(lastgen, npool):
                     print('Error: expected ' + geneName + ' to be in ANN results...')
                     pass
             if count > 0:
-                mean_prop = mean_prop / float(count)
+                if type(runtype) == list:
+                    mean_prop[0] = mean_prop[0]/float(count)
+                    mean_prop[1] = mean_prop[1]/float(count)
+                else:
+                    mean_prop = mean_prop / float(count)
             ## write
             _write_prop_csv(base_path, generation, mean_prop)
         generation += 1
