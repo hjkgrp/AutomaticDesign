6b218f0c68d8a609801027a9ec7705edb009b98e
Aditya Nandy <aditya.nandy@gmail.com>
2019-07-18 21:14:37 -0400

    Fixed dictionary return in ga_database_io

--- molSimplifyAD/ga_database_io.py
+++ molSimplifyAD/ga_database_io.py
@@ -22,18 +22,16 @@ def check_dbinputs(args_dict,step=False):
     dict_vars = keys_required
     if set(keys_required) <= set(args_dict.keys()):
         for key in keys_required:
-            globals().update({key: args_dict[key]})
+            locals().update({key: args_dict[key]})
     else:
         raise KeyError("missing necessary keys to push data. Required inputs are: ", keys_required)
-    if "publication" in args_dict.keys():
+    dict_vars += ["publication","update_fields"]
+    if "publication" in args_dict.keys() and (args_dict["publication"] != False):
         publication = args_dict["publication"]
-        dict_vars += ["publication"]
-    if "update_fields" in args_dict.keys():
+    if "update_fields" in args_dict.keys() and (args_dict["update_fields"] != False):
         update_fields = args_dict["update_fields"]
-        dict_vars += ["update_fields"]
     for var in dict_vars:
-        print('THIS IS', var)
-        db_keyword_dict.update({str(var): var})
+        db_keyword_dict.update({str(var): locals()[var]})
     return db_keyword_dict
     #return database, collection, tag, subtag, auth, publication, update_fields
 
@@ -48,6 +46,7 @@ def push_run(args):
     auth = db_keyword_dict['collection']
     publication = db_keyword_dict['publication']
     update_fields = db_keyword_dict['update_fields']
+    #print(database)
     #database, collection, tag, subtag, auth, publication, update_fields = check_dbinputs(args_dict)
     if auth:
         if (args.user and args.pwd):
@@ -89,7 +88,9 @@ def push_run_act_learn(args):
     _, all_runs, act_learn_dict_lists = check_all_current_convergence(post_all=True)
     ### act_learn_dict is list with 2 elements. First one is oxo, second one is hat
     ordering = ['oxo','hat'] #overwriting collection
-    for i, descriptor_dict_list in enumerate(act_learn_dict_lists):
-        push_complex_actlearn(step=step, all_complexes=descriptor_dict_list, database=database, collection=ordering[i],
+    push_complex_actlearn(step=step, all_complexes=descriptor_dict_list[0], database=database, collection="act_learn_oxo",
+                          user=user, pwd=pwd, host="localhost", port=27017,
+                          auth=auth)
+    push_complex_actlearn(step=step, all_complexes=descriptor_dict_list[1], database=database, collection="act_learn_hat",
                           user=user, pwd=pwd, host="localhost", port=27017,
                           auth=auth)
