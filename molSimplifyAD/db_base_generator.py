00994103d6eafe6cd57473c7b6fac94e79891a9c
Aditya Nandy <aditya.nandy@gmail.com>
2020-02-01 16:45:25 -0500

    Deprecated files

--- molSimplifyAD/db_base_generator.py
+++
@@ -1,2 +0,0 @@
-from sqlalchemy.ext.declarative import declarative_base, DeferredReflection
-Base= declarative_base()
--- molSimplifyAD/wake.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash 
-module load anaconda
-source activate jp
-python /home/jp/treetest/GA/check_on_jobs.py
